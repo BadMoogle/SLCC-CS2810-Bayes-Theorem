03d80d24fac6cf177e679eb93a04677b9e5ac29b
Daniel Harper <daniel.c.harper@live.com>
2021-08-27 18:24:39 -0600

    Initial Commit

---
+++ build/CMakeFiles/bayestheorem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bayestheorem.dir/main.cpp.o"
+  "bayestheorem.pdb"
+  "bayestheorem"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bayestheorem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
